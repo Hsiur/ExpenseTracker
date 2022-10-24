05777090c0a6947b80253617f2cfed4bfd5c45dd
Ruslan Ishmukhametov <ruish10@me.com>
2022-10-24 15:02:35 +0500

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Ruslan Ishmukhametov on 24.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
